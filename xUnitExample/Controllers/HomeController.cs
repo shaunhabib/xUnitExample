1e32707d9d60c5403560db5c6e8b09b2951a3ab9
Shaun Habib <habibshaun124@gmail.com>
2025-07-17 16:54:47 +0600

    action added in controller

--- xUnitExample/Controllers/HomeController.cs
+++ xUnitExample/Controllers/HomeController.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace xUnitExample.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/[controller]/[action]")]
     [ApiController]
     public class HomeController : ControllerBase
     {
