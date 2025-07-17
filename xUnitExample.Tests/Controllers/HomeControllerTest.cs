422f39ab38c9d094c7880039f258aa5ee9de17f4
Shaun Habib <habibshaun124@gmail.com>
2025-07-17 16:33:51 +0600

    Add project files.

---
+++ xUnitExample.Tests/Controllers/HomeControllerTest.cs
@@ -0,0 +1,38 @@
+﻿using xUnitExample.Controllers;
+
+namespace xUnitExample.Tests.Controllers
+{
+    public class HomeControllerTest
+    {
+        [Fact]
+        public void Get_ReturnsHelloWorld()
+        {
+            // Arrange
+            var controller = new HomeController();
+            string expectedResult = "Hello, World!";
+
+            // Act
+            var result = controller.GetWithFact();
+
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(expectedResult, result);
+        }
+
+        [Theory]
+        [InlineData(100, "You guessed correct number")]
+        [InlineData(50, "You guessed too low")]
+        [InlineData(150, "You guessed too high")]
+        public void GetWithTheory_ReturnsExpectedResult(int guessNumber, string expectedMessage)
+        {
+            // Arrange
+            var controller = new HomeController();
+            // Act
+            var result = controller.GetWithTheory(guessNumber);
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(expectedMessage, result);
+        }
+    }
+}
---
+++ xUnitExample/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace xUnitExample.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+
+        [HttpGet]
+        public string GetWithFact()
+        {
+            return "Hello, World!";
+        }
+
+        [HttpGet]
+        public string GetWithTheory(int guessnumber)
+        {
+            if (guessnumber == 100)
+            {
+                return "You guessed correct number";
+            }
+            else if (guessnumber < 100)
+            {
+                return "You guessed too low";
+            }
+            else
+            {
+                return "You guessed too high";
+            }
+        }
+    }
+}
