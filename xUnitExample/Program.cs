a640ecfb37511ebe524232b7e19cc87285b7c5cf
Shaun Habib <habibshaun124@gmail.com>
2025-07-17 16:48:17 +0600

    take swagger outside from the development

--- xUnitExample/Program.cs
+++ xUnitExample/Program.cs
@@ -12,9 +12,11 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseDeveloperExceptionPage();
 }
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
